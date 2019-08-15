a19084a469ad1a5bcad015b585baf79a3d4bc4e4
etimoz <x@xs-iMac.local>
2019-08-15 12:24:11 +0200

    added start service command

---
+++ HomeTemperature/management/commands/start_service.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+
+from HomeTemperature.temperature_service import TemperatureService
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('pin', type=int)
+        parser.add_argument('minutes', type=int)
+
+    def handle(self, *args, **options):
+        pin = options['pin']
+        time = options['minutes']
+        print(pin)
+        print(time)
+        if pin and time:
+            service = TemperatureService(sensor_pin=pin, minutes=time)
+        else:
+            service = TemperatureService()
+
+        service.start()
